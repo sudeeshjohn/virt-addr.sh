22762b6c091b6c6179acdaff004b72b50cdc4856
sudeeshjohn <sudheeshjohn@gmail.com>
2016-04-15 11:51:26 -0400

    directly $1 used

--- virt-addr.sh
+++ virt-addr.sh
@@ -9,6 +9,6 @@
 #   192.0.2.16
 #
 virt-addr() {
-    VM="$1"
-    arp -an | grep "`virsh dumpxml $VM | grep "mac address" | sed "s/.*'\(.*\)'.*/\1/g"`" | awk '{ gsub(/[\(\)]/,"",$2); print $2 }'
+    VM=$1
+    arp -an | grep "`virsh dumpxml $1 | grep "mac address" | sed "s/.*'\(.*\)'.*/\1/g"`" | awk '{ gsub(/[\(\)]/,"",$2); print $2 }'
 }
